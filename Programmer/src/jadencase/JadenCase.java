65f765dc732fdbc5b8f95e2c9e1e5ab6ab0e87ee
chltmdgh522 <chltmdgh517@naver.com>
2023-01-14 02:21:58 +0900

    Question Jadencase

---
+++ Programmer/src/jadencase/JadenCase.java
@@ -0,0 +1,19 @@
+package jadencase;
+
+public class JadenCase {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+        System.out.println((s.solution("ddd")));
+    }
+}
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        if (s.charAt(0) >= 97 && s.charAt(0) <= 122) {
+        }
+        return answer;
+    }
+}
