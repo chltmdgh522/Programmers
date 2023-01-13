693f4769659a09305b7e8dfe156ff76879948bcf
chltmdgh522 <chltmdgh517@naver.com>
2023-01-14 01:36:14 +0900

    Question collatzguess

---
+++ Programmer/src/collatzguess/CollatzGuess.java
@@ -0,0 +1,38 @@
+package collatzguess;
+
+public class CollatzGuess {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.print(s.solution(2));
+    }
+}
+
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        int cnt = 0;
+        if (num == 1) {
+            return answer;
+        } else if (num != 1) {
+            while (answer == 0) {
+                cnt++;
+                if (num % 2 == 0) {
+                    num /= 2;
+                } else if (num % 2 == 1) {
+                    num = num * 3 + 1;
+                }
+                if (num == 1) {
+                    answer = cnt;
+                }
+
+                if (cnt == 500) {
+                    answer = -1;
+                }
+            }
+        }
+        return answer;
+
+
+    }
+
+}
