349374b0bd291b617ab08e75602c84523ac58c8f
chltmdgh522 <chltmdgh517@naver.com>
2023-01-16 01:46:49 +0900

    Question jadencase

--- Programmer/src/jadencase/JadenCase.java
+++ Programmer/src/jadencase/JadenCase.java
@@ -2,18 +2,22 @@ package jadencase;
 
 public class JadenCase {
     public static void main(String[] args) {
-
         Solution s = new Solution();
-        System.out.println((s.solution("ddd")));
+        System.out.println((s.solution("for the last week ")));
     }
 }
-
 class Solution {
     public String solution(String s) {
         String answer = "";
+        String[] sp=s.split(" ");
+        for(int i=0; i< sp.length; i++){
+            answer+=sp[i].substring(0,1).toUpperCase();
+            answer+=sp[i].substring(1).toLowerCase();
+            answer+=" ";
 
-        if (s.charAt(0) >= 97 && s.charAt(0) <= 122) {
         }
-        return answer;
+
+        if(s.substring(s.length() -1, s.length()).equals(" ")) return answer;
+        return answer.substring(0, answer.length() - 1);
     }
 }
