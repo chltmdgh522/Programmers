79dfa3eb1799f5de0910404848f31d15778c9884
chltmdgh522 <chltmdgh517@naver.com>
2023-01-25 00:08:34 +0900

    Question lotto

--- Programmer/src/lotto/Lotto.java
+++ Programmer/src/lotto/Lotto.java
@@ -1,68 +1,83 @@
 package lotto;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Lotto {
     public static void main(String[] args) {
-        int[] lottos = {44, 1, 0, 0, 31, 25};
-        int[] win_nums = {31, 10, 45, 1, 6, 19};
+        int[] lottos = {45, 4, 35, 20, 3, 9};
+        int[] win_nums = {20, 9, 3, 45, 4, 35};
         Solution s = new Solution();
-        System.out.println(s.solution(lottos, win_nums));
+        int[] answer = s.solution(lottos, win_nums);
+        for(int i = 0; i < answer.length; i++){
+            System.out.print(answer[i]);
+        }
     }
 }
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] lottoEx = new int[6];
-        int[] answer = {};
-        int cnt = 0;
-        int cnte=0;
+        int[] answer = new int[2];
+        int min = 0;
+        int max=0;
         for (int i = 0; i < win_nums.length; i++) {
             for (int j = 0; j < lottos.length; j++) {
                 if (lottos[j] == win_nums[i]) {
                     lottoEx[i] += lottos[j];
-                    cnt++;
+                    min++;
                 }
             }
         }
-        Arrays.stream(lottoEx).sorted();
-        for (int i = 0; i < win_nums.length; i++) {
-            for (int j = 0; j < lottos.length; j++) {
-                if (lottos[j] == 0) {
-                    if (win_nums[i] != lottoEx[j]) {
-                        lottos[j] = win_nums[j];
-                        cnte++;
-                    }
-                }
+        for(int i=0; i<lottos.length; i++){
+            if(lottos[i]==0){
+                max++;
             }
         }
-        cnte+=cnt;
-        for (int i = 0; i < lottos.length; i++) {
-            System.out.println(lottos[i]);
+        max+=min;
+        //Arrays.sort(lottoEx);
+
+        switch(max){
+            case 6:
+                answer[0]=1;
+                break;
+            case 5:
+                answer[0]=2;
+                break;
+            case 4:
+                answer[0]=3;
+                break;
+            case 3:
+                answer[0]=4;
+                break;
+            case 2:
+                answer[0]=5;
+                break;
+            case 1,0:
+                answer[0]=6;
+                break;
+        }
+        switch(min){
+            case 6:
+                answer[1]=1;
+                break;
+            case 5:
+                answer[1]=2;
+                break;
+            case 4:
+                answer[1]=3;
+                break;
+            case 3:
+                answer[1]=4;
+                break;
+            case 2:
+                answer[1]=5;
+                break;
+            case 1,0:
+                answer[1]=6;
+                break;
         }
 
-        for(int i=0; i<answer.length; i++){
-            System.out.println(answer[i]);
-        }
-
-        switch(cnt){
-            case 6: answer[0]=1;
-            case 5: answer[0]=2;
-            case 4: answer[0]=3;
-            case 3: answer[0]=4;
-            case 2: answer[0]=5;
-            case 1: answer[0]=6;
-            case 0: answer[0]=6;
-        }
-        switch(cnte){
-            case 6: answer[1]=1;
-            case 5: answer[1]=2;
-            case 4: answer[1]=3;
-            case 3: answer[1]=4;
-            case 2: answer[1]=5;
-            case 1: answer[1]=6;
-            case 0: answer[1]=6;
-        }
 
         return answer;
     }
