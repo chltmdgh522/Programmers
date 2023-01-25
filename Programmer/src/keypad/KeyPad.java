ec668898b7fa7e9379fba635589f21a54f8f6342
chltmdgh522 <chltmdgh517@naver.com>
2023-01-26 00:04:40 +0900

    Question keypad

---
+++ Programmer/src/keypad/KeyPad.java
@@ -0,0 +1,19 @@
+package keypad;
+
+public class KeyPad {
+    public static void main(String[] args) {
+        int[] numbers={1,2,3,4,5,6,7,8,9,0};
+        String hand="right";
+        Solution s=new Solution();
+        System.out.println(s.solution(numbers, hand));
+    }
+
+}
+
+class Solution{
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        
+        return answer;
+    }
+}
