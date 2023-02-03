0e87643f73673f986217783fddd67da0a0295106
chltmdgh522 <chltmdgh517@naver.com>
2023-02-04 00:12:04 +0900

    Question phonelist

---
+++ Programmer/src/phonelist/PhoneList.java
@@ -0,0 +1,22 @@
+package phonelist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PhoneList {
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        Solution s = new Solution();
+        System.out.println(s.solution(phone_book));
+    }
+}
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add
+        return answer;
+
+    }
+}
