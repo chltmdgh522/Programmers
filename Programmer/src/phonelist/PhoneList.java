40097e05dabba71b93c78641dfd9678971ae8b51
chltmdgh522 <chltmdgh517@naver.com>
2023-02-05 00:38:39 +0900

    Question phonelist

--- Programmer/src/phonelist/PhoneList.java
+++ Programmer/src/phonelist/PhoneList.java
@@ -1,11 +1,10 @@
 package phonelist;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class PhoneList {
     public static void main(String[] args) {
-        String[] phone_book = {"119", "97674223", "1195524421"};
+        String[] phone_book = {"119", "97674223", "135524421"};
         Solution s = new Solution();
         System.out.println(s.solution(phone_book));
     }
@@ -13,10 +12,18 @@ public class PhoneList {
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Set<String> hashSet = new HashSet<>();
-        hashSet.add
-        return answer;
+        Map<String, Integer> map = new HashMap<>();
+        String a = phone_book[0];
+        // 2. 모든 전화번호를 HashMap에 넣는다.
+        for (int i = 0; i < phone_book.length; i++)
+            map.put(phone_book[i], i);
+        // 3. 모든 전화번호의 substring이 HashMap에 존재하는지 확인한다.
+        for (String s : phone_book)
+            for (int j = 0; j < s.length(); j++)
+                if (map.containsKey(s.substring(0, j)))
+                    return false;
 
+        return true;
+         
     }
 }
