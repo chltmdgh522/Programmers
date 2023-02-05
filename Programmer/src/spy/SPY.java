36aaa70abbdc87b652ff8d61836ae804821e3471
chltmdgh522 <chltmdgh517@naver.com>
2023-02-06 00:27:46 +0900

    Question SPY

--- Programmer/src/spy/SPY.java
+++ Programmer/src/spy/SPY.java
@@ -6,19 +6,20 @@ import java.util.Map;
 
 public class SPY {
     public static void main(String[] args) {
-        String[][] clothes={{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        Solution s =new Solution();
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        Solution s = new Solution();
         System.out.println(s.solution(clothes));
 
     }
 }
+
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 0;
-        Map<String, Integer> hashMap=new HashMap<>();
-        for(String[] s: clothes){
-            String type=s[1];
-            hashMap.put(type,hashMap.getOrDefault(type,0)+1 );
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (String[] s : clothes) {
+            String type = s[1];
+            hashMap.put(type, hashMap.getOrDefault(type, 0) + 1);
         }
 
 
