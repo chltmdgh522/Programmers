d27ecb61598bcedb3ac8bd353fd84e6c0bf89032
chltmdgh522 <chltmdgh517@naver.com>
2023-02-06 00:27:31 +0900

    Question SPY

---
+++ Programmer/src/spy/SPY.java
@@ -0,0 +1,28 @@
+package spy;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class SPY {
+    public static void main(String[] args) {
+        String[][] clothes={{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        Solution s =new Solution();
+        System.out.println(s.solution(clothes));
+
+    }
+}
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        Map<String, Integer> hashMap=new HashMap<>();
+        for(String[] s: clothes){
+            String type=s[1];
+            hashMap.put(type,hashMap.getOrDefault(type,0)+1 );
+        }
+
+
+        System.out.println(hashMap);
+        return answer;
+    }
+}
