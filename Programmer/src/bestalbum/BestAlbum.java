11a1d0a29a6fd314a4d28d2af6e9b8e264b61fa6
chltmdgh522 <chltmdgh517@naver.com>
2023-02-13 00:06:22 +0900

    Question bestalbum

---
+++ Programmer/src/bestalbum/BestAlbum.java
@@ -0,0 +1,27 @@
+package bestalbum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BestAlbum {
+    public static void main(String[] args) {
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+        Solution s = new Solution();
+        int[] answer = s.solution(genres, plays);
+        for (int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+}
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        Map<String,Integer> hashMap= new HashMap<>();
+
+
+        System.out.println(hashMap);
+
+        return answer;
+    }
+}
