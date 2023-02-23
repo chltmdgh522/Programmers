34d0b8be5a604056295bb1a8ba957222a8fb8729
chltmdgh522 <chltmdgh517@naver.com>
2023-02-23 13:59:01 +0900

    Question bestalbum space modify

--- Programmer/src/bestalbum/BestAlbum.java
+++ Programmer/src/bestalbum/BestAlbum.java
@@ -16,15 +16,16 @@ public class BestAlbum {
         }
     }
 }
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
-        Map<String,Integer> hashMap= new HashMap<>();
-        for(int i=0; i<genres.length; i++){
-            hashMap.put(genres[i],hashMap.getOrDefault(genres[i],0)+plays[i]);
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
+            hashMap.put(genres[i], hashMap.getOrDefault(genres[i], 0) + plays[i]);
         }
-        ArrayList<String> genre=new ArrayList<>();
-        for(String key : hashMap.keySet()){
+        ArrayList<String> genre = new ArrayList<>();
+        for (String key : hashMap.keySet()) {
             genre.add(key);
         }
 
