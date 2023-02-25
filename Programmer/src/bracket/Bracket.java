b6f98443988755ffb2c3c28b123b78f3a6680d14
chltmdgh522 <chltmdgh517@naver.com>
2023-02-25 18:40:06 +0900

    Question bracket

---
+++ Programmer/src/bracket/Bracket.java
@@ -0,0 +1,18 @@
+package bracket;
+
+public class Bracket {
+    public static void main(String[] args) {
+        String s = "()()";
+        Solution solution = new Solution();
+        System.out.println(solution.solution(s));
+
+    }
+}
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        return answer;
+    }
+}
