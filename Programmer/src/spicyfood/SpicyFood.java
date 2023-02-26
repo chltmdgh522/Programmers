fd1a5164e4c6e6f7149a728d3ae2837c1690b3e7
chltmdgh522 <chltmdgh517@naver.com>
2023-02-27 00:14:57 +0900

    Question spicyfood

---
+++ Programmer/src/spicyfood/SpicyFood.java
@@ -0,0 +1,20 @@
+package spicyfood;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class SpicyFood {
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int k = 7;
+        Solution s = new Solution();
+        System.out.println(s.solution(scoville,k));
+    }
+
+}
+
+class Solution {
+    public int solution(int[] scoville, int k) {
+        int answer = 0;
+        return answer;
+    }
+}
