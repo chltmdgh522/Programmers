2a9df4f46bb0b2f0fb220a1e5e5299a413aebbf9
chltmdgh522 <chltmdgh517@naver.com>
2023-03-06 00:01:49 +0900

    Question maxnum

--- Programmer/src/maxnum/MaxNum.java
+++ Programmer/src/maxnum/MaxNum.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class MaxNum {
     public static void main(String[] args) {
         int[] numbers = {3, 30, 34, 5, 9};
-        Solution s= new Solution();
+        Solution s = new Solution();
         System.out.println(s.solution(numbers));
 
     }
@@ -13,22 +13,22 @@ public class MaxNum {
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer="";
-        String[] num=new String[numbers.length];
-        for(int i=0; i<numbers.length; i++){
-            num[i]=String.valueOf(numbers[i]);
+        String answer = "";
+        String[] num = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            num[i] = String.valueOf(numbers[i]);
         }
         Arrays.sort(num, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                return (o2+o1).compareTo(o1+o2);
+                return (o2 + o1).compareTo(o1 + o2);
             }
         });
 
-        if(num[0].equals("0"))
+        if (num[0].equals("0"))
             return "0";
-        for(String s:num){
-            answer+=s;
+        for (String s : num) {
+            answer += s;
         }
         return answer;
     }
