f933f9a251a74c9974178dcc5a43f1c5b5c7b938
chltmdgh522 <chltmdgh517@naver.com>
2023-03-06 23:46:30 +0900

    Question physicalcloth

---
+++ Programmer/src/physicalcloth/PhysicalCloth.java
@@ -0,0 +1,19 @@
+package physicalcloth;
+
+public class PhysicalCloth {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        Solution s = new Solution();
+        System.out.println(s.solution(n, lost, reserve));
+    }
+}
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        return answer;
+    }
+}
