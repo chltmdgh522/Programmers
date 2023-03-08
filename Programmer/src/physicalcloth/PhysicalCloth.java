f41d213476295454ff13b731ba6e55ea8a6e4363
chltmdgh522 <chltmdgh517@naver.com>
2023-03-09 00:30:34 +0900

    Question physicalcloth

--- Programmer/src/physicalcloth/PhysicalCloth.java
+++ Programmer/src/physicalcloth/PhysicalCloth.java
@@ -1,10 +1,13 @@
 package physicalcloth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhysicalCloth {
     public static void main(String[] args) {
         int n = 5;
         int[] lost = {2, 4};
-        int[] reserve = {1, 3, 5};
+        int[] reserve = {3};
         Solution s = new Solution();
         System.out.println(s.solution(n, lost, reserve));
     }
@@ -12,9 +15,28 @@ public class PhysicalCloth {
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        List<Integer> list = new ArrayList<>();
+        for (int k = 0; k < lost.length; k++) {
+            list.add(lost[k]);
+        }
+
         int answer = 0;
+        for (int i = 0; i < reserve.length; i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (reserve[i] + 1 == list.get(j)) {
+                    answer++;
+                    list.remove(j);
+                    break;
+                } else if (reserve[i] - 1 == list.get(j)) {
+                    answer++;
+                    list.remove(j);
+                    break;
+                }
+            }
+        }
+        answer += reserve.length;
 
         return answer;
-        
+
     }
 }
