d4dd70435019552d40530186e9afb056087b76bc
seung_ho_choi.s <chltmdgh517@naver.com>
2023-05-28 21:55:19 +0900

    PracticeExam

--- Programmer/src/PracticeExam/PracticeExam.java
+++ Programmer/src/PracticeExam/PracticeExam.java
@@ -40,6 +40,7 @@ class Solution {
                 sortedEntries.add(entry);
             }
         }
+        
         Collections.sort(sortedEntries, new Comparator<Map.Entry<Integer, Integer>>() {
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
                 return o2.getValue().compareTo(o1.getValue());
